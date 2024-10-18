8c54d145f7373b5430784295bf30f944ce3e94ba
Ahsannyc <ahsannyc@yahoo.com>
2024-10-18 00:29:09 -0500

    abcd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import Footer from "./components/footer"
-import Header from "./components/Header"
-import Hero from "./components/Hero"
-import Products from "./components/Products"
-import Sale from "./components/Sale"
+import Footer from "../components/footer"
+import Header from "../components/Header"
+import Hero from "../components/Hero"
+import Products from "../components/Products"
+import Sale from "../components/Sale"
 
 function Homepage(){
 return(
